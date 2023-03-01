7732c0d9cc01e741229f7e6cd5b357f12d533d20
M Disha <85287773+Disha0803@users.noreply.github.com>
2023-03-01 23:47:34 +0530

    Day - 16

---
+++ Logical Programming using Python/08_singleinheritance.py
@@ -0,0 +1,10 @@
+#single inheritance
+class Parent:
+    def fun1(self):
+        print("Hi.... this is Parent")  
+class Child(Parent):
+    def fun2(self):
+        print("Hi...this is Child")
+obj=Child() 
+obj.fun1()
+obj.fun2()
---
+++ Logical Programming using Python/10_hierarchicalinheritance.py
@@ -0,0 +1,12 @@
+#hierarchical inheritance
+class father:
+    def fun(self):
+        print("Father Property")
+class son(father):
+    pass
+class daughter(father):
+    pass
+obj=son()
+obj2=daughter()
+obj.fun()
+obj2.fun() 
